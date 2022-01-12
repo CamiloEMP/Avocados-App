75fff8d341a9f2297168923c5b62c188f8f8d0ff
b7c8p <morapaezc@gmail.com>
2022-01-12 08:03:25 -0500

    change name project

---
+++ components/NavBar/index.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export const NavBar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <Link>
+          <a href=""></a>
+          </Link>
+          <a href=""></a>
+        </ul>
+      </nav>
+    </div>
+  )
+}
